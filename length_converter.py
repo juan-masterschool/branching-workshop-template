8383776ad6fcf2650e9a795f918741b68796fd44
Martin Haferanke <martin.haferanke@gmail.com>
2025-05-28 12:12:18 +0200

    - Adding meters_to_kilometers function to calculate meters to kilometers

---
+++ length_converter.py
@@ -0,0 +1,11 @@
+
+METERS_PER_KILOMETER = 1000
+
+def meters_to_kilometers(meters: float) -> float:
+    """
+    Converts a length from meters to kilometers.
+
+    :param meters: Length in meters.
+    :return: Equivalent length in kilometers.
+    """
+    return meters / METERS_PER_KILOMETER

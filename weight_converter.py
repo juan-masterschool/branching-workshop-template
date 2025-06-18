366ae56d0ef95b99cd39f88447a7051423705cae
Sandras Mac <sandra@air-von-sandras.speedport.ip>
2025-06-18 13:57:04 +0200

    create weight_converter.py with some comments

---
+++ weight_converter.py
@@ -0,0 +1,7 @@
+def grams_to_kilograms():
+    '''
+    converts grams to kilograms
+    :param grams: float number of a weight in grams
+    :return: float number of the converting kilometers
+    '''
+    pass

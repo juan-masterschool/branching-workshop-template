508971bfcae1e6fbcbb5a9f5685ae8eb4d5fc6d7
wbkay71 <wanja.kneib@googlemail.com>
2025-06-18 13:00:31 +0200

    define square_meters_to_square_kilometers function in area_converter

---
+++ area_converter_wanja.py
@@ -0,0 +1,5 @@
+def square_meters_to_square_kilometers(square_meters):
+    square_kilometers = square_meters / 1_000_000
+    print(f"{square_meters} square meters = {square_kilometers} square kilometers")
+    return square_kilometers
+square_meters_to_square_kilometers(2500000000)

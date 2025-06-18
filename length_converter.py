234db2a847ebcf694947593b3bc4989213eafc59
Janis Löns <janis.loens@outlook.de>
2025-06-18 13:04:33 +0200

    Created meters_to_kilometer

---
+++ length_converter.py
@@ -0,0 +1,9 @@
+def meters_to_kilometers():
+    """
+    Converrt meters to kilometers.
+    """
+    meters = float(input("Enter the length in meters: "))
+    kilometers = meters / 1000
+    print(f"{meters} meters is equal to {kilometers} kilometers.")
+
+meters_to_kilometers()

1f42210d7063d9831ded8e3464a65a9381fe3086
Xaradur <felixgloess@protonmail.com>
2025-06-25 12:18:47 +0200

    Add function to convert seconds to minutes in time_converter.py

---
+++ time_converter.py
@@ -0,0 +1,11 @@
+def seconds_to_minutes(seconds):
+    """Converts a time value from seconds to minutes."""
+    return seconds / 60
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
